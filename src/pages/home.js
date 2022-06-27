4a008ca6fb420a8ec8b1351aac6d3906b7cf964b
vikram54 <vikram@it-waves.com>
2022-06-27 14:04:42 +0530

    minnor fix

--- src/pages/home.js
+++ src/pages/home.js
@@ -22,19 +22,20 @@ const Home = () => (
     <section className="seo_features_one bg_color sec_pad">
       <div className="container">
         <div className="row flex-row-reverse">
-          <div className="col-lg-6">
+          <div className="col-lg-7">
             <div className="seo_features_img">
-              <img src={feature_img} alt="features_img" />
+              <img className="img-fluid" src={feature_img} alt="features_img" />
             </div>
           </div>
-          <div className="col-lg-6">
+          <div className="col-lg-5">
             <Fade bottom cascade>
               <div className="seo_features_content ">
                 <h2>ENGAGE,ACQUIRE & RETAIN CUSTOMERS</h2>
-                <h3>
-                  with Gift Cards, Store credit,<br></br> Refferral and Loyalty
-                  program
-                </h3>
+                <p>
+                  Enhance your customer's journey with Gift card, In-store
+                  credit, and Loyalty programs and inspire them to share their
+                  passion for your brand with refferals.
+                </p>
                 <div className="col-lg-9">
                   <form action="#" className="contact_form_box" method="post">
                     <div className="form-group text_box">
@@ -45,13 +46,15 @@ const Home = () => (
                         placeholder="Email"
                       />
                     </div>
-                    <button
-                      className="btn btn_get btn_get_two mt-3"
-                      type="submit"
-                    >
+                    <button className="btn btn_get btn_get_two" type="submit">
                       Get Started for Free
                     </button>
                   </form>
+                </div>
+                <div className="row mt_15 ">
+                  <li className="col "> No Credit Card Reaquired</li>
+                  <li className="col"> Free Sign Up</li>
+                  <li className="col"> 24/7 Support</li>
                 </div>
                 <div className={`partner_logo_area_four `}>
                   <h4 className="f_size_18 f_bold f_p  l_height28 ">
