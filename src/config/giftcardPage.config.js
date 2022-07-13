a081d4431b6d840bf4bf5d9927d3849aa5a14d3b
vikram54 <vikram@it-waves.com>
2022-07-13 13:35:17 +0530

    update the content

--- src/config/giftcardPage.config.js
+++ src/config/giftcardPage.config.js
@@ -1,57 +1,61 @@
 import Icons from "../shared/assets";
 
-export const RELOAD_LIST = [
+export const BULK_GIFTCARD_LIST = [
   {
     title: "",
-    description: " Reload Gift card @store, @online",
+    description:
+      " Generate gift cards numbers for printing physical plastic gift card",
   },
   {
     title: "",
-    description: "Add Promotional $ value to Gift card from Admin Panel",
-  },
-];
-
-export const UPLOAD_PHOTO_LIST = [
-  {
-    title: "",
-    description: " Reload Gift card @store, @online",
-  },
-  {
-    title: "",
-    description: "Add Promotional $ value to Gift card from Admin Panel",
-  },
-];
-
-export const SKU_LIST = [
-  {
-    title: "",
-    description: " Reload Gift card @store, @online",
-  },
-  {
-    title: "",
-    description: "Add Promotional $ value to Gift card from Admin Panel",
-  },
-];
-
-export const MOBILE_WALLET_LIST = [
-  {
-    title: "",
-    description: " Reload Gift card @store, @online",
-  },
-  {
-    title: "",
-    description: "Add Promotional $ value to Gift card from Admin Panel",
+    description: "Generate gift cards numbers for Corporate Gifting",
   },
 ];
 
 export const MIGRATION_LIST = [
   {
     title: "",
-    description: " Reload Gift card @store, @online",
+    description:
+      "Import existing gift cards | voucher | Certificates balance into 99minds.",
   },
   {
     title: "",
-    description: "Add Promotional $ value to Gift card from Admin Panel",
+    description:
+      "Seamless migration from existing gift card processor to 99minds",
+  },
+];
+
+export const MULTICURRENCY_LIST = [
+  {
+    title: "",
+    description:
+      " Supports 100+ currencies, including USD, CAD, EUR, GBP, KRW, JPY, CNY & many more!",
+  },
+];
+
+export const MULTI_STORE_LIST = [
+  {
+    title: "",
+    description: "Gift card can be redeemed at multiple locations",
+  },
+];
+
+export const RETURN_REFUND_LIST = [
+  {
+    title: "",
+    description:
+      " Gift card can be issue for merchandise return. Gift card amount can be updated on merchandise return bought on gift card.",
+  },
+];
+export const WALLET_LIST = [
+  {
+    title: "",
+    description: " Gift card can be store in digital wallet.",
+  },
+  {
+    title: "",
+    description:
+      " Intuitive iOS / Android mobile wallet pass allows for a frictionless consumer experience at checkout in-store and online.",
   },
 ];
 
@@ -59,56 +63,65 @@ export const GIFTCARD_TABS_CONFIG = {
   HEAD: [{ Title: "" }],
   PANNEL_DATA: [
     {
-      id: "reload",
-      label: "Reload/Top Up",
+      id: "bulk_gift_card",
+      label: "Bulk Gift Card ",
       selected: "true",
       image: Icons.Reload.default,
-      title: "Reload/Top Up Gift Card ",
+      title: "Bulk Gift Card ",
       description1: "",
       description2: "",
-      list: RELOAD_LIST,
-    },
-    {
-      id: "uploadPhoto",
-      label: "Upload Photo & Video ",
-      selected: "false",
-      image: Icons.Upload.default,
-      title: "Upload Photo & Video",
-      description1: "",
-      description2: "",
-      list: UPLOAD_PHOTO_LIST,
-    },
-    {
-      id: "sku",
-      label: "SKU Based Gift Card",
-      selected: "false",
-      image: Icons.Reload.default,
-      title: "SKU Based Gift Card",
-      subTitle: "",
-      description1: "",
-      description2: "",
-      list: SKU_LIST,
-    },
-
-    {
-      id: "mobile",
-      label: "Mobile Wallet Pass",
-      image: Icons.Reload.default,
-      title: "Mobile Wallet Pass",
-      description1: "  ",
-      description2: "",
-      list: MOBILE_WALLET_LIST,
+      list: BULK_GIFTCARD_LIST,
     },
     {
       id: "migration",
-      label: "Gift Card Migration",
-      image: Icons.Reload.default,
-      title: "Gift Card Migration",
-      subTitle: "",
+      label: "Migration",
+      selected: "false",
+      image: Icons.Upload.default,
+      title: "Migration",
       description1: "",
       description2: "",
       list: MIGRATION_LIST,
     },
+    {
+      id: "multicurrency",
+      label: "Multicurrency",
+      selected: "false",
+      image: Icons.Reload.default,
+      title: "Multicurrency",
+      subTitle: "",
+      description1: "",
+      description2: "",
+      list: MULTICURRENCY_LIST,
+    },
+
+    {
+      id: "multi-Store",
+      label: "Multi-Store",
+      image: Icons.Reload.default,
+      title: "Multi-Store",
+      description1: "  ",
+      description2: "",
+      list: MULTI_STORE_LIST,
+    },
+    {
+      id: "return_refund",
+      label: "Return & Refund",
+      image: Icons.Reload.default,
+      title: "Return & Refund",
+      subTitle: "",
+      description1: "",
+      description2: "",
+      list: RETURN_REFUND_LIST,
+    },
+    {
+      id: "Wallet",
+      label: "Wallet",
+      image: Icons.Reload.default,
+      title: "Wallet",
+      subTitle: "",
+      description1: "",
+      description2: "",
+      list: WALLET_LIST,
+    },
   ],
 };
-
