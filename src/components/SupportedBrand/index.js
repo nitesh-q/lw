2808bb0eac52c449b37d86d55f086733d0d58e37
vikram54 <vikram@it-waves.com>
2022-07-18 13:38:20 +0530

    minnor update

--- src/components/SupportedBrand/index.js
+++ src/components/SupportedBrand/index.js
@@ -10,17 +10,9 @@ const items = [
 const SupportedBrand = ({ id, pClass, fclass, imgClass }) => {
   return (
     <section id="supportedBrand">
-      {/* <div className="container"> */}
       <div className={`partner_logo_area_four  ${pClass}`}>
         <h3 className={`${fclass} `}>We Support</h3>
         <div className="row  partner_info">
-          {/* {items.map((i, k) => {
-          return (
-            <div className="logo_item " data-wow-delay="0.1s">
-              <img src={i.image} alt={k} />
-            </div>
-          );
-        })} */}
           <div className={`logo_item w_100 ${imgClass} `} data-wow-delay="0.1s">
             <img className="h_18" src={Icons.Miva_Logo.default} alt="" />
             <img className="h_18" src={Icons.Miva.default} alt="" />
@@ -36,13 +28,12 @@ const SupportedBrand = ({ id, pClass, fclass, imgClass }) => {
             <img className="h_25" src={Icons.Shopify_Logo.default} alt="" />
             <img className="h_25" src={Icons.Shopify.default} alt="" />
           </div>
-          <div className={`logo_item w_110 ${imgClass}`} data-wow-delay="0.1s">
+          <div className={`logo_item w_128 ${imgClass}`} data-wow-delay="0.1s">
             <img className="h_25" src={Icons.Woo_Logo.default} alt="" />
             <img className="h_25" src={Icons.Woo.default} alt="" />
           </div>
         </div>
       </div>
-      {/* </div> */}
     </section>
   );
 };
